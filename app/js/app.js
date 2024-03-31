11c6aeeec1cc82173a7faf051a121445e43dc09b
Cameliya <nastavasina3682@gmail.com>
2024-03-31 18:48:24 +0300

    Initial commit

---
+++ app/js/app.js
@@ -0,0 +1,27 @@
+// // Import vendor jQuery plugin example
+// import '~/app/libs/mmenu/dist/mmenu.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// Custom JS
+
+})
+
+function showAdditionalCircle() {
+	const additionalCircle = document.querySelector('.additional-circle');
+	additionalCircle.classList.toggle('hidden');
+  }
+
+
+
+
+/*const agreeButton = document.getElementById('agreeButton');
+const agreementCheckbox = document.getElementById('agreementCheckbox');
+
+agreeButton.addEventListener('click', function() {
+    if (agreementCheckbox.style.display === 'none') {
+        agreementCheckbox.style.display = 'inline-block';  // Показываем галочку
+    } else {
+        agreementCheckbox.style.display = 'none';  // Скрываем галочку
+    }
+});*/
